c28b6c2e7a309540b0712527df7dc17c0aacceeb
Jenn Marshall <jennyfever@gmail.com>
2022-11-09 14:39:01 -0700

    debug localstorage and remove excess comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,12 @@
-var info1 = $(".btn");
-var lastFive = []
-info1.on("click", function(){
-    if(lastFive.length < 5)
-    lastFive.push($("#lastSearch").val())
-    localStorage.setItem("previosSearches", lastFive);
-  })
-
-var requestUrl = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin";
+var lastFive = [];
 var searchButton = $("#search-btn");
 var historyButton = $("#history-btn");
 var ingredient = $("#search-input");
-
-// $.ajax({
-//   url: requestUrl,
-//   method: "GET",
-// }).then(function (response) {
-//   console.log("Ajax response \n-------------");
-//   console.table(response);
-// });
-
-// fetch(requestUrl)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
-
 const drinkUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=";
 
 function searchDrink() {
   var ingredientnow = ingredient.val();
-  //  
+  //
   console.log(ingredientnow);
   var searchDrinkUrl = drinkUrl + ingredientnow;
   console.log(searchDrinkUrl);
@@ -46,58 +21,21 @@ function searchDrink() {
     // var resultButton = $("<button>");
     for (let i = 0; i < 5; i++) {
       console.log(drinks[i]);
+      //   TODO: populate value of results buttons with Drink names
     }
   });
 }
 
-searchButton.on("click", searchDrink);
-// historyButton.on("click", searchDrink);
-// function () {
-//   var ingredientnow = ingredient.val() || $(this).val();
-//   //   ingredientnow is either going to be the input of the search text area (when clicked by button near search text input)
-//   // or is going to be the value of the button that is clicked
-
-//   //   var ingredientnow = function () {
-//   //     if ($(this).siblings("input")) {
-//   //       return $(this).siblings("input").val();
-//   //     } else {
-//   //       return $(this).val();
-//   //     }
-//   //   };
-//   console.log(ingredientnow);
-//   var searchDrinkUrl = drinkUrl + ingredientnow;
-//   console.log(searchDrinkUrl);
-
-//   $.ajax({
-//     url: searchDrinkUrl,
-//     method: "GET",
-//     crossDomain: true,
-//   }).then(function (response) {
-//     console.log(response);
-//   });
-// });
-
-// function searchQuery() {
-//   var ingredient = $("#search-input").val();
-//   console.log(ingredient);
-//   return ingredient;
-// }
-
-// ingredient = searchQuery();
-
-// console.log(ingredient);
-
-// function searchDrinkApi() {
-//   var drinkUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=";
-//   drinkUrl = drinkUrl + ingredient;
-//   console.log(drinkUrl);
-
-//   $.ajax({
-//     url: drinkUrl,
-//     method: "GET",
-//   }).then(function (response) {
-//     console.log(response);
-//   });
-// }
-
-// searchDrinkApi();
+searchButton.on("click", function () {
+  console.log("goodjob");
+  console.log($("#search-input").val());
+  console.log(lastFive.length);
+  //   adds ingredient to lastFive array, saves to localstorage
+  if (lastFive.length < 5) {
+    lastFive.push($("#search-input").val());
+    localStorage.setItem("previousSearches", lastFive);
+    console.log(lastFive);
+  }
+  //   runs function to search the API
+  searchDrink();
+});
